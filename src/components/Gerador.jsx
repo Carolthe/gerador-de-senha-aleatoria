d7a66a36334bc7aeae604cc1d1a48297d106c8fa
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-10-13 16:47:08 +0100

    gerador-de-senha-aleatoria

--- src/components/Gerador.jsx
+++ src/components/Gerador.jsx
@@ -2,23 +2,31 @@ import { useState } from "react"
 
 export default function Gerador (){
     const [gerar, setGerar] = useState("")
+    const [inputNumeros, setInputNumeros] = useState (8)
 
     function gerarSenhas(){
         const caracteres = "QWERTYUIOPASDFGHJKLÇZXCVBNM!#$%&?@"
         let newPaddword = ""
-        for (let i = 0; i< 12; i++){
+        for (let i = 0; i < inputNumeros; i++){
             newPaddword += caracteres.charAt(Math.floor(Math.random() * caracteres.length))
         }
         setGerar(newPaddword)
     }
     function apagarSenha (){
         setGerar("")
+        setInputNumeros("")
     }
+
     return(
         <>
         <h1>Gerador de Senha Aleatória</h1>
+        <input 
+        value={inputNumeros}
+        onChange={(ev) => setInputNumeros(ev.target.value)}
+        type="number"
+         />
         <button
-        onClick={gerarSenhas}>Gerar</button>
+        onClick={gerarSenhas}>Gerar {inputNumeros}</button>
        
         <p>{gerar}</p>
         {gerar &&
